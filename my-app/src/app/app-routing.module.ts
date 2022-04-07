f5b5f97d08a75193648857530fb0db0c25940263
Nathanael Huh <nathanaelhuh@Nathanaels-MacBook-Pro-2.local>
2022-04-07 13:27:33 -0600

    added account page with basic login

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ShopComponent } from './shop/shop.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { AccountComponent } from './account/account.component';
 
 
 const routes: Routes = [
 	{ path: 'shop', component: ShopComponent },
 	{ path: 'home', component: HomeComponent },
 	{ path: 'about', component: AboutComponent },
+	{ path: 'account', component: AccountComponent },
 ];
 
 @NgModule({
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ShopComponent } from './shop/shop.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AboutComponent } from './about/about.component';
     ShopComponent,
     NavbarComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
