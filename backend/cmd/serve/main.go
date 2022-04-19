2b2d30246fff477c85d62319764976d64debd56e
Zachary <mhkoo@ucalgary.ca>
2022-04-18 18:00:15 -0600

    edited some commands

--- backend/cmd/serve/main.go
+++ backend/cmd/serve/main.go
@@ -98,7 +98,7 @@ func main() {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "localhost")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		next.ServeHTTP(w, r)
 	})
 }
